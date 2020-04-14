940e2048803332c211796c82dc4ea70fa720e24b
lin <lin@bglen.jp>
2020-04-15 00:13:29 +0900

    removed submodule

---
+++ frontend/vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "devServer": {
+    "proxy": "http://groupware.local"
+  },
+  "outputDir": "../public",
+  "indexPath": "index.html",
+  "transpileDependencies": [
+    "vuetify"
+  ]
+}
